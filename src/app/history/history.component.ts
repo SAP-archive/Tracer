131e66a094dff5e7cd9bf9f3dd00fc3895002fd7
eran.ofer <eran.ofer@sap.com>
2019-12-10 09:27:42 +0200

    limit history expansion in case of a long input

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -81,6 +81,22 @@ export class HistoryComponent {
       this.setting.SetHistoryRecords(this.Items);
     }
   }
+
+  text_truncate = function (str, length, ending) {
+    if (length == null) {
+      length = 100;
+    }
+    if (ending == null) {
+      ending = '...';
+    }
+    if (str.length > length) {
+      return str.substring(0, length - ending.length) + ending;
+    } else {
+      return str;
+    }
+  };
+
+
 }
 
 // tslint:disable-next-line: class-name
